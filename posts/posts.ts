2a6a52f88b00892173a52a66e0b081ef51d85bc6
Zerapium <kimtaehyung2451982@gmail.com>
2021-12-02 20:07:26 +0530

    Implement 'getAllPosts, exists' methods

--- posts/posts.ts
+++ posts/posts.ts
@@ -9,26 +9,47 @@ class Posts extends Db {
     static createPost(title: string, body: Dict<BlogBody>, author: string): string {
         if (Object.keys(body).length < 1) return "ERROR : The post body is empty";
 
+        let output = "";
         try {
             let postData: any = {};
             postData.title = title;
             postData.author = author;
+            postData.body = body;
 
             let post = new Post(postData);
             if (!this.data.posts) this.data.posts = {};
             this.data.posts[post.id] = post;
             this.exportDatabase("posts");
-            return post.id;
+            console.log("post added");
+            output = post.id;
         } catch (e: any) {
             console.log(e);
         }
 
-        finally {
-            return "";
-        }
+     return output;
 
     }
 
+
+    static getAllPosts() : Array<PostType> {
+
+        let keys = Object.keys(this.data.posts);
+        let posts : Array<PostType> = [];
+        keys.forEach((key) => {
+            posts.push(this.data.posts[key]);
+        });
+        return posts;
+    }
+
+    static get(id : string) : PostType {
+        return this.data.posts[id.trim()];
+    }
+
+    static exists(id : string) : boolean {
+        if(!this.data.posts[id.trim()]) return false;
+        return true;
+    }
+ 
 }
 
 export default Posts;
