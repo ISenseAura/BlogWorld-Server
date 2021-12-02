91ab2ef91eb23abc6e485fc9d74c794d9a7f6f17
Zerapium <kimtaehyung2451982@gmail.com>
2021-12-02 14:43:53 +0530

    Set Headers to allow CORS

--- server.ts
+++ server.ts
@@ -5,6 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', 1);
+
+    next();
+});
+
 app.use("/api/auth", require("./routes/auth"));
 
 
