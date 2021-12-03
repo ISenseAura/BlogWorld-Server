8c8ae83f8b66c2046ac1c9c471b18481447488be
Glitch (glistening-lackadaisical-glue) <none>
2021-12-03 08:28:16 +0000

    🛋🎿 Checkpoint ./server.ts:758933/11

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
 
     res.setHeader('Access-Control-Allow-Credentials', 1);
 
