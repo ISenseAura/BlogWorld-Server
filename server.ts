431f352dfa7975fa8e50d66e93edcf123fdd0dbf
Glitch (glistening-lackadaisical-glue) <none>
2022-01-26 07:17:43 +0000

    🍇🌸 Checkpoint ./server.ts:758933/1077 ./package.json:758933/12

--- server.ts
+++ server.ts
@@ -18,7 +18,24 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+
 app.use("/api/auth", require("./routes/auth"));
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,auth-token');
+
+    res.setHeader('Access-Control-Allow-Credentials', 1);
+
+    next();
+});
+
+
 app.use("/api/posts", require("./routes/posts"));
 
 
